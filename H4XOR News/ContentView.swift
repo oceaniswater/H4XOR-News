bee2790381e26eb669634afa7a54613646ad86ef
markgolubev <markflyingbear@gmail.com>
2023-02-02 15:44:23 +0000

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by Марк Голубев on 02.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
